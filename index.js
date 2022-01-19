7f95bf17e9f45d188cbf7606948ff9bd1ea288ec
utpal641 <utpalsarker250@gmail.com>
2022-01-19 13:52:19 +0600

    js grade

---
+++ index.js
@@ -0,0 +1,27 @@
+var numbers=80;
+
+switch(numbers){
+    case 100:
+        console.log('Grade is A+');
+        break;
+    case 80 :
+        console.log('Grade is A+');
+        break;
+    case 70:
+        console.log('Grade is A');
+        break;
+    case 60 :
+        console.log('Grade is A-');
+        break;
+    case 50:
+        console.log('Grade is B');
+        break;
+    case 40 :
+        console.log('Grade is C');
+        break;
+    default:
+        console.log('this is fail');
+}
+
+
+
